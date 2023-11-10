6535f5f9417c8ca3b4fd5417c2b15ad2d03052f6
hossein <Hossein.m.7782@gmail.com>
2023-11-10 19:57:07 +0330

    create admin and forms

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    gender= models.CharField(choices=('Man','Female'), max_length=5)
+    GENDER_CHOICES=(
+        ('Man','Man'),
+        ('Female','Female'),
+        )
+
+    gender= models.CharField(choices=GENDER_CHOICES, max_length=7,null=False, blank=False)
     age= models.PositiveIntegerField(null=True, blank=True)
